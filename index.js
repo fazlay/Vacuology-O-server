00a74fa547409f2d585787b358f3c1e4d5aac576
Fazlay Rabbi <rfazlay21@gmail.com>
2021-11-12 03:55:07 +0600

    delete and approve order

--- index.js
+++ index.js
@@ -62,12 +62,74 @@ async function run() {
       res.json(result);
     });
 
-    //---------------------------------------------------------order post
+    //---------------------------------------------------------get All Order
+    app.get('/orders', async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
+    //--------------------------------------------------------- submit order  post
     app.post('/orders', async (req, res) => {
       const orders = req.body;
       console.log(orders);
       const result = await ordersCollection.insertOne(orders);
-      res.json('text');
+      res.json(result);
+    });
+
+    //--------------------------------------------------------- get specifiq user order
+    app.get('/orders/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const cursor = ordersCollection.find(query);
+      const userOrder = await cursor.toArray();
+      res.json(userOrder);
+    });
+    //----------------------------------order Approve
+    app.put('/orders/:id', async (req, res) => {
+      const orderId = req.params.id;
+      const updateStatus = req.body;
+      console.log(updateStatus.status);
+      const options = { upsert: true };
+      const filter = { _id: ObjectId(orderId) };
+      const updateDoc = {
+        $set: { status: updateStatus.status },
+      };
+      const result = await ordersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+    //--------------------------------order Delete
+    app.delete('/orders/:id', async (req, res) =>{
+      const id =req.params.id
+      console.log(id);
+      const query ={_id:ObjectId(id)}
+      const result = await ordersCollection.deleteOne(query)
+      res.json(result)
+    })
+
+    //-----------------------------------------------------admin creat
+    app.put('/user/admin', async (req, res) => {
+      const user = req.body;
+      console.log(user.adminMail);
+      const filter = { email: user.adminMail };
+      const updateDoc = { $set: { role: 'admin' } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result);
+    });
+    app.get('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+
+      let isAdmin = false;
+      if (user?.role === 'admin') {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
     });
   } finally {
   }
