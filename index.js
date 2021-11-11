7c50db71c096bb7b89f77d04095de9a980ef1b18
Fazlay Rabbi <rfazlay21@gmail.com>
2021-11-11 21:13:11 +0600

    put users

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
-const ObjectId =require('mongodb').ObjectId
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-
 
 //vacuadmin 8iUimVBeAoDiBrGd
 
@@ -22,31 +21,54 @@ async function run() {
     await client.connect();
     const database = client.db('vacuology');
     const productsCollection = database.collection('products');
+    const ordersCollection = database.collection('orders');
+    const usersCollection = database.collection('users');
 
-    app.get('/products', async(req, res) => {
+    app.get('/products', async (req, res) => {
       const cursor = productsCollection.find({});
       const products = await cursor.toArray();
       res.send(products);
     });
 
     //---------------------------------------------------------single Product
-    // app.get('/products1/:id' async(req,res)=>{
-
-    //   const id =req.param.id;
-    //   console.log(id);
-    //   res.send('text')
-    // })
-
-    //
-    app.get('/products/:id', async(req, res) => {
-      const productId=req.params.id
-    const query= {_id:ObjectId(productId)}
-    const singleProduct = await productsCollection.findOne(query)
+    app.get('/products/:id', async (req, res) => {
+      const productId = req.params.id;
+      const query = { _id: ObjectId(productId) };
+      const singleProduct = await productsCollection.findOne(query);
 
       res.send(singleProduct);
     });
 
-    
+    //-----------------------------------------------insert-Update User info in Server
+
+    app.post('/user', async (req, res) => {
+      const userInfo = req.body;
+      const result = await usersCollection.insertOne(userInfo);
+      res.json(result);
+    });
+    app.put('/user', async (req, res) => {
+      const usersInfo = req.body;
+      console.log(userInfo);
+      const filter = { email: usersInfo.email };
+      const options = { upsrt: true };
+      const updateDoc = {
+        $set: usersInfo,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    //---------------------------------------------------------order post
+    app.post('/orders', async (req, res) => {
+      const orders = req.body;
+      console.log(orders);
+      const result = await ordersCollection.insertOne(orders);
+      res.json('text');
+    });
   } finally {
   }
 }
